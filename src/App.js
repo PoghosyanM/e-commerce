00a002f3d5615d4377e059592ccf8c194a5ab068
Mihran <poghosyan.mihran.96@gmail.com>
2020-04-09 15:05:52 +0400

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ClassTesting from './ClassTesting';
+import FunctionalTesting from './FunctionalTesting';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalTesting />
+    </div>
+  );
+}
+
+export default App;
